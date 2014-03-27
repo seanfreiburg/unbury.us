7f0550fbb70a72fc1a4dcd4d5c971c77de54d57f
Sean Freiburg <frycicle@gmail.com>
2014-03-27 00:03:41 -0500

    adding switch to fields

--- new_design/assets/js/loan_controller.js
+++ new_design/assets/js/loan_controller.js
@@ -9,7 +9,9 @@ var add_loan = function () {
     $("#loan" +id).hide().fadeIn();
     window.loans[id] = new Loan(id,0,0,0,0);
     add_loan_destroy_listener(id);
-    add_loan_input_listener(id,"loan-name");
+    add_loan_input_listeners(id);
+
+
 
 
 };
@@ -28,8 +30,25 @@ var add_loan_destroy_listener = function (id) {
     });
 };
 
+var add_loan_input_listeners = function(id){
+    add_loan_input_listener(id,"loan-name");
+    add_loan_input_listener(id,"current-balance");
+    add_loan_input_listener(id,"minimum-payment");
+    add_loan_input_listener(id,"interest-rate");
+};
+
 var add_loan_input_listener = function(id,field_name){
     $("#loan" +id).find("input[name=" +field_name + "]").focusout( function() {
-        console.log(id);
+        console.log(field_name);
+        switch (field_name) {
+            case "loan-name":
+                break;
+            case "current-balance":
+                break;
+            case "minimum-payment":
+                break;
+            case "interest-rate":
+                break;
+        }
     });
 };
