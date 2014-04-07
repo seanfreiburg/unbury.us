9103447706645b81c1b5b9faaab82a5cf52b013c
Sean Freiburg <frycicle@gmail.com>
2014-04-07 00:07:05 -0500

    sort of snowball and avalanche

--- new_design/assets/js/results_controller.js
+++ new_design/assets/js/results_controller.js
@@ -1,17 +1,16 @@
-var ResultsController = function() {
+var ResultsController = function () {
 
 };
 
 
-ResultsController.results = function() {
+ResultsController.results = function () {
     var results = ResultsController.compute_results();
     ResultsController.draw_total_results(results);
     ResultsController.draw_loan_results(results);
 };
 
 
-
-ResultsController.draw_total_results = function(results){
+ResultsController.draw_total_results = function (results) {
     var source = $("#total-results-template").html();
     var template = Handlebars.compile(source);
     var html = template(results);
@@ -19,23 +18,42 @@ ResultsController.draw_total_results = function(results){
     $("#total-results").hide().fadeIn('500');
 };
 
-ResultsController.draw_loan_results= function(results){
+ResultsController.draw_loan_results = function (results) {
     var source = $("#loan-results-template").html();
     var template = Handlebars.compile(source);
     var html = template(results);
     $("#loan-results").empty().append(html);
     $("#loan-results").hide().fadeIn('500');
-    for(var i = 0; i < results.loans.length; i++){
+    for (var i = 0; i < results.loans.length; i++) {
         Router.add_loan_table_result_listener(results.loans[i].id);
     }
 };
 
 
-ResultsController.compute_results = function(){
-   /*return {year: 2016, total_interest: 20.23, loans: [{id: 1, loan_name: "fg",year: 2016, total_interest: 20.23,
-       rows: [{month: "January", year: 2014,payment: 20, principal_paid: 17, interest_paid: 3, principal: 340,
-           total_interest: 23 }] }]};*/
-    return window.loans;
+ResultsController.compute_results = function () {
+    /*return {year: 2016, total_interest: 20.23, loans: [{id: 1, loan_name: "fg",year: 2016, total_interest: 20.23,
+     rows: [{month: "January", year: 2014,payment: 20, principal_paid: 17, interest_paid: 3, principal: 340,
+     total_interest: 23 }] }]};*/
+
+    var loans_dict = window.loans;
+    var loans = [];
+    for (index in loans_dict) {
+        loans.push(loans_dict[index]);
+    }
+    if (window.payment_type == "avalanche") {
+        loans.sort(function (a, b) {
+            return b.get_interest_rate() - a.get_interest_rate();
+        });
+    }
+    else {
+        console.log("snowball");
+        loans.sort(function (a, b) {
+            return a.currentBalance - b.currentBalance;
+        });
+        console.log(loans);
+    }
+
+    return loans;
 
 };
 
--- new_design/test_data.js
+++ new_design/test_data.js
@@ -1,10 +1,10 @@
 $().ready(function () {
+    window.payment_type = "snowball";
     window.loans = {};
-    window.loans[1] = new Loan(1, 0, 0, 0, 0);
-    window.loans[2] = new Loan(2, 0, 0, 0, 0);
-    window.loans[3] = new Loan(3, 0, 0, 0, 0);
+    window.loans[1] = new Loan(1, 0, 500, 0, 4);
+    window.loans[2] = new Loan(2, 0, 400, 0, 3);
+    window.loans[3] = new Loan(3, 0, 300, 0, 2);
     var data = ResultsController.compute_results();
-    console.log(data);
     $('#data-out').append(JSON.stringify(data, undefined, 2));
 
 });
