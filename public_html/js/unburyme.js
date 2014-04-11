c5ce43025e1258a2c1bc3069365b10ffab5d0f30
Sean Freiburg <frycicle@gmail.com>
2014-04-10 22:07:44 -0500

    new_design now main

--- public_html/js/unburyme.js
+++
@@ -1,9 +0,0 @@
-/**
- * unbury.me - loan calculator
- * Namespace for Unburyme
- * http://www.github.com/jsantell/unburyme
- * @license The MIT License, Copyright (c) 2011 Jordan Santell
- * @author jsantell@gmail.com (Jordan Santell)
- */
-
-var Unburyme = Unburyme || {}
