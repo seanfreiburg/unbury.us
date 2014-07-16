047eae518c6a4ade6328212263d345c645d1f818
Sean Freiburg <sefreibu@microsoft.com>
2014-07-15 17:26:24 -0700

    starting the load from url code

--- public_html/assets/src/js/init.js
+++ public_html/assets/src/js/init.js
@@ -1,4 +1,33 @@
 $().ready(function () {
+
+    var params = getSearchParameters();
+    console.log(params);
+    if (params.empty()) {
+        load_empty_loans();
+    }
+    else {
+        load_url_loans(params);
+    }
+
+});
+
+
+// http://localhost:63342/unbury.us/public_html/index.html?name_0=1&balance_0=2&payment_0=4&rate_0=2
+var url_loans_valid = function (params) {
+
+};
+
+var load_url_loans = function (params) {
+    if (url_loans_valid(params)) {
+
+    }
+    else {
+        load_empty_loans();
+    }
+};
+
+
+var load_empty_loans = function () {
     window.loans = {};
     window.auto_increment = -1;
     window.monthly_payment = 0;
@@ -14,5 +43,23 @@ $().ready(function () {
     Router.add_calculate_listener();
 
     Handlebars.registerPartial("row", $("#loan-table-row-partial").html());
-});
+};
+
+
+var getSearchParameters = function () {
+    var prmstr = window.location.search.substr(1);
+    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+};
+
+var transformToAssocArray = function (prmstr) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for (var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+};
+
+
 
