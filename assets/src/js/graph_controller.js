858a7fdf64a143de1c31bde749e85fc7e617b898
Sean Freiburg <frycicle@gmail.com>
2014-12-14 17:33:15 -0800

    fixes in

--- assets/src/js/graph_controller.js
+++ assets/src/js/graph_controller.js
@@ -12,27 +12,27 @@ GraphController.graph = function (results) {
         var canvas_html = "<canvas id=\"myChart\"  height=\"400\"></canvas>";
         graph_div.append(title + canvas_html);
     }
-     var steps = GraphController.get_steps();
+    var steps = GraphController.get_steps();
 
-    respChart($("#myChart"), data,false, steps, GraphController.get_step_width(results,steps));
+    respChart($("#myChart"), data, false, steps, GraphController.get_step_width(results, steps));
 };
 
 GraphController.get_steps = function () {
     return 20;
 };
 
-GraphController.get_step_width = function (results,steps) {
-    var max_val = 0
+GraphController.get_step_width = function (results, steps) {
+    var max_val = 0;
     for (var i = 0; i < results.loans.length; i++) {
-        for (var j = 0; j < results.loans[i].rows.length; j++) {
-         if (max_val < parseFloat(results.loans[i].rows[j].principal_remaining)){
-             max_val = parseFloat(results.loans[i].rows[j].principal_remaining);
-         }
+        for (var j = 0; j < results.loans[i].length; j++) {
+            if (max_val < parseFloat(results.loans[i][j].principal_remaining)) {
+                max_val = parseFloat(results.loans[i][j].principal_remaining);
+            }
         }
 
     }
 
-    return Math.round(max_val/steps);
+    return Math.round(max_val / steps);
 
 };
 
@@ -40,25 +40,25 @@ GraphController.get_data = function (results) {
     var max_row_length = 0;
     var max_row_index = 0;
     var datasets = [];
-    results.loans.sort( function(a,b){
-        return b.currentBalance - a.currentBalance;
-    });
+    //results.loans.sort( function(a,b){
+    //    return b.currentBalance - a.currentBalance;
+    //});
     for (var i = 0; i < results.loans.length; i++) {
-        if (results.loans[i].rows.length > max_row_length) {
-            max_row_length = results.loans[i].rows.length;
+        if (results.loans[i].length > max_row_length) {
+            max_row_length = results.loans[i].length;
             max_row_index = i;
         }
         datasets[i] = GraphController.get_color_hash(i);
         datasets[i].data = [];
-        for (var j = 0; j < results.loans[i].rows.length; j++) {
-            datasets[i].data.push(results.loans[i].rows[j].principal_remaining);
+        for (var j = 0; j < results.loans[i].length; j++) {
+            datasets[i].data.push(results.loans[i][j].principal_remaining);
         }
     }
     var labels = [];
 
 
-    for (var i = 0; i < results.loans[max_row_index].rows.length; i++) {
-        labels.push(results.loans[max_row_index].rows[i].date);
+    for (var i = 0; i < results.loans[max_row_index].length; i++) {
+        labels.push(results.loans[max_row_index][i].date);
     }
 
     console.log(datasets);
--- assets/src/js/results_controller.js
+++ assets/src/js/results_controller.js
@@ -19,24 +19,48 @@ ResultsController.results = function () {
 ResultsController.draw_total_results = function (results) {
     var source = $("#total-results-template").html();
     var template = Handlebars.compile(source);
-    var html = template(results);
+    var html = template(results.totals);
     $("#total-results").empty().append(html);
     $("#total-results").hide().fadeIn('500');
 };
 
 ResultsController.draw_loan_results = function (results) {
-
-
     var source = $("#loan-results-template").html();
     var template = Handlebars.compile(source);
     var html = template(results);
     $("#loan-results").empty().append(html);
     $("#loan-results").hide().fadeIn('500');
-    for (var i = 0; i < results.loans.length; i++) {
-
-        Router.add_loan_table_result_listener(results.loans[i].id);
+    for (var loan_key in results.loans) {
+        console.log(loan_key);
+        Router.add_loan_table_result_listener(loan_key);
     }
 };
+
+
+/*
+ Loan step by step
+
+ Generate interest
+ Pay min payment
+ *Pay interest
+ *Pay principal
+ If leftover money, add to extra_payment
+ Remove loans at 0
+ Apply extra payment to loans until extra payment is 0 or remaining_loans is 0
+ Sort based on payment type
+ Remove loans as paid
+ Increment month
+
+ Loop above until remaining loans.size is 0
+
+ */
+
+ResultsController.loan_results = {"loans": {}, "totals": {} };
+
+//dicts and arrays are passed by reference
+// .add(1,'months');
+// .format("MMMM YYYY")
+// need to move current_interest to current_principal
 
 
 ResultsController.compute_results = function () {
@@ -47,139 +71,272 @@ ResultsController.compute_results = function () {
     for (var index in loans_dict) {
         loans.push(loans_dict[index]);
     }
-    if (window.payment_type == "avalanche") {
-        loans.sort(function (a, b) {
-            return b.interestRate - a.interestRate;
-        });
-    }
-    else {
-        loans.sort(function (a, b) {
-            return a.currentBalance - b.currentBalance;
-        });
-    }
 
-
-    return ResultsController.calculate(loans);
+    return ResultsController.calculate(loans, window.payment_type, window.monthly_payment);
 
 };
 
+ResultsController.calculate = function (loans, payment_type, monthly_payment) {
+    ResultsController.loan_results = {"loans": {}, "totals": {} };
+    var remaining_loans = {};
+    var current_principal = {};
+    var current_interest = {};
+    var minimum_payments = {};
+    var month = moment().startOf('month');
 
-/**
- * Calculates all current values -- does a brunt of the work
- * TODO This is a nightmare and needs cleaned up
- * @param {number[]} sortedLoans An array of the UID of loans
- */
-
-/**
- * This is not my code. The previous person made this a cluster
- * Sean
- */
-
-
-ResultsController.calculate = function (sortedLoans) {
-
-    var current_month = new UDate(),
-        principal_remaining_array = [],
-        interest_rate_array = [],
-        minimum_payment_array = [],
-        interest_paid_array = [],
-        principal_paid_array = [],
-        monthly_payment_array = [],
-        finished_loans_array = [],
-        num_remaining_loans = sortedLoans.length,
-        starting_loan_count = sortedLoans.length,
-        possible_calculation = true;
-
-    // Initialize and draw
-    for (var i = 0; i < starting_loan_count; i++) {
-        sortedLoans[i].total_interest_paid = 0;
-        sortedLoans[i].rows = [];
-        principal_remaining_array[i] = sortedLoans[i].currentBalance;
-        interest_rate_array[i] = (sortedLoans[i].interestRate * 0.01);
-        minimum_payment_array[i] = sortedLoans[i].minimumPayment;
-        finished_loans_array[i] = 0;
-        sortedLoans[i].rows.push({principal_remaining: principal_remaining_array[i].toFixed(2), principal_paid: 0, balance: principal_remaining_array[i].toFixed(2), payment: 0, interest_paid: 0, date: current_month.print() });
+    for (var loan_key in loans) {
+        ResultsController.loan_results.loans[loan_key] = {"rows": [], "loan_name": loans[loan_key].loanName,
+            "total_date": "0", "total_interest_paid": 0, "id": loan_key};
+        current_principal[loan_key] = loans[loan_key].currentBalance;
+        current_interest[loan_key] = 0;
+        remaining_loans[loan_key] = loans[loan_key];
+        ResultsController.log_default_line(loan_key, month);
+        var current_balance = current_principal[loan_key] + current_interest[loan_key];
+        ResultsController.log_balance_remaining_line(loan_key, current_balance);
 
     }
-    while ((num_remaining_loans != 0) && possible_calculation) {
-        current_month.increment();
-        var monthlyPayment = window.monthly_payment;
-        var firstPaymentPass = true;
-        //Set monthly payments
-        for (var h = 0; h < starting_loan_count; h++) {
-            if (finished_loans_array[h] == 0) {
-                interest_paid_array[h] = (principal_remaining_array[h] * (interest_rate_array[h] / 12));
-                principal_paid_array[h] = 0;
-                sortedLoans[h].total_interest_paid += interest_paid_array[h];
-                monthly_payment_array[h] = minimum_payment_array[h];
-                monthlyPayment = monthlyPayment - minimum_payment_array[h];
-            }
-        }
-        // Loops to ensure all focus payment is distributed
-        while ((monthlyPayment > 0 || firstPaymentPass) && num_remaining_loans) {
-            // Give rollover/focus money to priority loans
-            for (var j = 0; j < starting_loan_count; j++) {
-                if (monthlyPayment > 0 && principal_remaining_array[j] > 0) {
-                    monthly_payment_array[j] += monthlyPayment;
-                    monthlyPayment = 0;
-                }
-            }
-            // Payment pass
-            for (var k = 0; k < starting_loan_count; k++) {
-                if (principal_remaining_array[k] > 0) {
-                    if (monthly_payment_array[k] - interest_paid_array[k] < principal_remaining_array[k]) {
-                        principal_paid_array[k] = monthly_payment_array[k] - interest_paid_array[k];
-                        principal_remaining_array[k] -= principal_paid_array[k];
-                    }
-                    else {
-                        principal_paid_array[k] = principal_remaining_array[k];
-                        monthlyPayment = monthly_payment_array[k] - principal_remaining_array[k] - interest_paid_array[k];
-                        monthly_payment_array[k] = principal_remaining_array[k] + interest_paid_array[k];
-                        principal_remaining_array[k] = 0;
-                        num_remaining_loans -= 1;
-                        sortedLoans[k].rows.push({principal_remaining: principal_remaining_array[k].toFixed(2), principal_paid: principal_paid_array[k].toFixed(2), balance: principal_remaining_array[k].toFixed(2), payment: Number(monthly_payment_array[k]).toFixed(2), interest_paid: interest_paid_array[k].toFixed(2), date: current_month.print() });
 
-                    }
-                }
-            }
-            firstPaymentPass = false;
-        } // while ((focusPayment>0 || firstPaymentPass) && remainingLoans)
 
-        for (var m = 0; m < starting_loan_count; m++) {
-            if (principal_remaining_array[m] > 0) {
-                console.log(current_month.print());
-                sortedLoans[m].rows.push({principal_remaining: principal_remaining_array[m].toFixed(2), principal_paid: principal_paid_array[m].toFixed(2), balance: principal_remaining_array[m].toFixed(2), payment: Number(monthly_payment_array[m]).toFixed(2), interest_paid: interest_paid_array[m].toFixed(2), date: current_month.print() });
-            }
+    //loop
+    while (Object.keys(remaining_loans).length > 0) {
+        month.add(1, 'months');
+        var extra_payment = monthly_payment;
+        for (var loan_key in remaining_loans) {
+            minimum_payments[loan_key] = remaining_loans[loan_key].minimumPayment;
+            extra_payment = extra_payment - minimum_payments[loan_key];
+            ResultsController.log_default_line(loan_key, month)
+        }
+        // pay minimums first
+        ResultsController.pay_minimums(remaining_loans, current_principal, current_interest, minimum_payments);
+        extra_payment = extra_payment + ResultsController.add_leftover_payments(minimum_payments);
+        ResultsController.remove_paid_off_loans(remaining_loans, current_principal, current_interest, minimum_payments);
+        ResultsController.apply_extra_payments(remaining_loans, current_principal, current_interest, payment_type, extra_payment, minimum_payments);
+        for (var loan_key in remaining_loans) {
+            var current_balance = current_principal[loan_key] + current_interest[loan_key];
 
+            ResultsController.log_balance_remaining_line(loan_key, current_balance);
         }
-        for (var g = 0; g < starting_loan_count; g++) {
-            if (finished_loans_array[g] == 0) {
-                if (principal_remaining_array[g] == 0) {
-                    finished_loans_array[g] = 1;
-                    sortedLoans[g].date = current_month.print();
-                }
-            }
-        }
-        // Check to see current date is < year 2200 to prevent ridiculous calls
-        if (current_month.getYear() > 2200) {
-            console.log("hit");
-            possible_calculation = false;
-        }
-    }// while(remainingLoans)
-    if (possible_calculation) {
-        var total_interest_paid = 0;
-        for (var n = 0; n < starting_loan_count; n++) {
-            total_interest_paid += sortedLoans[n].total_interest_paid;
-            sortedLoans[n].total_interest_paid = sortedLoans[n].total_interest_paid.toFixed(2);
-        }
-        return {loans: sortedLoans, total_interest: total_interest_paid.toFixed(2), year: current_month.print() };
+
+        ResultsController.add_interest(remaining_loans, current_principal, current_interest);
+
     }
-    else {
-        console.log("impossible");
-        return null;
-    }
+
+    ResultsController.log_totals(month);
+    return ResultsController.loan_results;
 
 };
+
+ResultsController.remove_paid_off_loans = function (remaining_loans, current_principal, current_interest, minimum_payments) {
+    for (var loan_key in remaining_loans) {
+        var current_balance = current_principal[loan_key] + current_interest[loan_key];
+        if (current_balance == 0) {
+            ResultsController.remove_loan(remaining_loans, current_principal, current_interest, minimum_payments, loan_key);
+        }
+    }
+};
+
+ResultsController.remove_loan = function (remaining_loans, current_principal, current_interest, minimum_payments, loan_key) {
+    delete remaining_loans[loan_key];
+    delete current_principal[loan_key];
+    delete current_interest[loan_key];
+    delete minimum_payments[loan_key];
+};
+
+ResultsController.pay_minimums = function (remaining_loans, current_principal, current_interest, minimum_payments) {
+    for (var loan_key in remaining_loans) {
+        var current_balance = current_principal[loan_key] + current_interest[loan_key];
+        var left_over = ResultsController.apply_payment(loan_key, minimum_payments[loan_key], current_interest, current_principal);
+        minimum_payments[loan_key] = left_over;
+
+    }
+};
+
+
+ResultsController.apply_payment = function (id, payment, interest_dict, principal_dict) {
+
+    // interest higher than payment
+    if (interest_dict[id] > payment) {
+        ResultsController.log_interest_paid_line(id, payment);
+        interest_dict[id] = interest_dict[id] - payment;
+        return 0;
+    }
+    // interest equal to payment
+    else if (interest_dict[id] == payment) {
+        ResultsController.log_interest_paid_line(id, payment);
+        interest_dict[id] = interest_dict[id] - payment;
+        return 0;
+    }
+    // payment higher than interest left
+    else {
+        ResultsController.log_interest_paid_line(id, interest_dict[id]);
+        var remaining_payment = payment - interest_dict[id];
+        interest_dict[id] = 0;
+        // apply to principal
+        return ResultsController.principal_apply_payment(id, remaining_payment, principal_dict);
+    }
+};
+
+ResultsController.principal_apply_payment = function (id, payment, principal_dict) {
+
+    // principal higher than payment
+    if (principal_dict[id] > payment) {
+        ResultsController.log_principal_paid_line(id, payment);
+        principal_dict[id] = principal_dict[id] - payment;
+        return 0;
+    }
+    // principal equal to payment
+    else if (principal_dict[id] == payment) {
+        ResultsController.log_principal_paid_line(id, payment);
+        principal_dict[id] = principal_dict[id] - payment;
+        return 0;
+    }
+    // payment higher than principal left
+    else {
+
+        ResultsController.log_principal_paid_line(id, principal_dict[id]);
+        var remaining_payment = payment - principal_dict[id];
+        principal_dict[id] = 0;
+        return remaining_payment;
+    }
+};
+
+//assuming monthly
+ResultsController.add_interest = function (remaining_loans, current_principal, current_interest) {
+    for (var loan_key in remaining_loans) {
+        var current_balance = current_principal[loan_key] + current_interest[loan_key];
+        //assuming monthly
+        var interest_generated = current_balance * (remaining_loans[loan_key].interestRate / 100 / 12);
+        current_interest[loan_key] = current_interest[loan_key] + ResultsController.precise_round(interest_generated, 2);
+    }
+};
+
+ResultsController.apply_extra_payments = function (remaining_loans, current_principal, current_interest, payment_type, extra_payment, minimum_payments) {
+    var sorted_keys = ResultsController.sort_loans(remaining_loans, current_principal, current_interest, payment_type);
+    for (var key in sorted_keys) {
+        if (extra_payment == 0 || Object.keys(remaining_loans).length == 0) {
+            return extra_payment;
+        }
+        extra_payment = ResultsController.apply_payment(sorted_keys[key], extra_payment, current_interest, current_principal);
+        if ((current_principal[sorted_keys[key]] + current_interest[sorted_keys[key]]) == 0) {
+            ResultsController.remove_loan(remaining_loans, current_principal, current_interest, minimum_payments, sorted_keys[key]);
+        }
+    }
+};
+
+
+ResultsController.add_leftover_payments = function (minimum_payments) {
+    var leftover = 0;
+    for (var key in minimum_payments) {
+        leftover += minimum_payments[key];
+    }
+    return leftover;
+};
+
+
+ResultsController.precise_round = function (num, decimals) {
+    var t = Math.pow(10, decimals);
+    return parseFloat((Math.round((num * t) + (decimals > 0 ? 1 : 0) * (Math.sign(num) * (10 / Math.pow(100, decimals)))) / t).toFixed(decimals));
+};
+
+ResultsController.sort_loans = function (remaining_loans, current_principal, current_interest, payment_type) {
+    if (payment_type == "snowball") {
+        return Object.keys(remaining_loans).sort(function (a, b) {
+            return (current_interest[a] + current_principal[a]) - (current_interest[b] + current_principal[b])
+        });
+    }
+    else if (payment_type == "avalanche") {
+        return Object.keys(remaining_loans).sort(function (a, b) {
+            return (remaining_loans[b].interestRate) - (remaining_loans[a].interestRate)
+        });
+    }
+    else {
+        console.log("something fucky happened")
+    }
+};
+
+
+// not sure where I used this
+ResultsController.round_interest_diff = function (a) {
+    if (a > 0) {
+        a = Math.ceil(a);
+    }
+    else if (a < 0) {
+        a = Math.floor(a);
+
+    }
+    else {
+        return a;
+    }
+};
+
+ResultsController.arraysEqual = function (a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    // If you don't care about the order of the elements inside
+    // the array, you should sort both arrays here.
+
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+};
+
+
+// loan_results = { "0": [line, line], "1": [line, line] }
+// line = {month, payment,principal_paid,interest_paid,balance_remaining}
+ResultsController.log_default_line = function (id, date) {
+    ResultsController.loan_results.loans[id].rows.push({});
+    ResultsController.loan_results.loans[id].rows[ResultsController.loan_results.loans[id].rows.length - 1]["date"] = date.format("MMMM YYYY");
+    ResultsController.loan_results.loans[id].rows[ResultsController.loan_results.loans[id].rows.length - 1]["payment"] = 0;
+    ResultsController.loan_results.loans[id].rows[ResultsController.loan_results.loans[id].rows.length - 1]["principal_paid"] = 0;
+    ResultsController.loan_results.loans[id].rows[ResultsController.loan_results.loans[id].rows.length - 1]["interest_paid"] = 0;
+    ResultsController.loan_results.loans[id].rows[ResultsController.loan_results.loans[id].rows.length - 1]["balance_remaining"] = 0;
+};
+
+
+ResultsController.log_date_line = function (id, date) {
+    ResultsController.loan_results.loans[id].rows[ResultsController.loan_results.loans[id].rows.length - 1]["date"] = date.format("MMMM YYYY");
+};
+
+ResultsController.log_payment_line = function (id, payment) {
+    ResultsController.loan_results.loans[id].rows[ResultsController.loan_results.loans[id].rows.length - 1]["payment"] += ResultsController.precise_round(payment, 2);
+};
+
+ResultsController.log_principal_paid_line = function (id, principal_paid) {
+    ResultsController.loan_results.loans[id].rows[ResultsController.loan_results.loans[id].rows.length - 1]["principal_paid"] += ResultsController.precise_round(principal_paid, 2);
+    //console.log(id + " " + principal_paid);
+    ResultsController.log_payment_line(id, principal_paid);
+};
+
+ResultsController.log_interest_paid_line = function (id, interest_paid) {
+
+    ResultsController.loan_results.loans[id].rows[ResultsController.loan_results.loans[id].rows.length - 1]["interest_paid"] += ResultsController.precise_round(interest_paid, 2);
+    ResultsController.log_payment_line(id, interest_paid);
+};
+
+ResultsController.log_balance_remaining_line = function (id, balance_remaining) {
+    ResultsController.loan_results.loans[id].rows[ResultsController.loan_results.loans[id].rows.length - 1]["balance_remaining"] += ResultsController.precise_round(balance_remaining, 2);
+};
+
+ResultsController.log_totals = function (date) {
+    ResultsController.loan_results.totals["total_date"] = date.format("MMMM YYYY");
+    var total_interest_paid = 0;
+    for (var loan_key in  ResultsController.loan_results.loans) {
+        ResultsController.loan_results.loans[loan_key].total_interest_paid = 0;
+        for (var line_key in ResultsController.loan_results.loans[loan_key].rows) {
+            ResultsController.loan_results.loans[loan_key].total_interest_paid += ResultsController.loan_results.loans[loan_key].rows[line_key]["interest_paid"];
+        }
+        ResultsController.loan_results.loans[loan_key].total_interest_paid = ResultsController.precise_round(ResultsController.loan_results.loans[loan_key].total_interest_paid, 2);
+        ResultsController.loan_results.loans[loan_key].total_date = ResultsController.loan_results.loans[loan_key].rows[ResultsController.loan_results.loans[loan_key].rows.length - 1]["date"];
+        total_interest_paid += ResultsController.loan_results.loans[loan_key].total_interest_paid
+    }
+    ResultsController.loan_results.totals["total_interest_paid"] = total_interest_paid;
+};
+
+
+
 
 
 
